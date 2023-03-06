88cb2ea4805e6a062774381b4fc74b3c9e7268ff
Fadel-Dev <diopfadel06@gmail.com>
2023-03-06 00:51:43 +0000

    Initial commit

---
+++ src/Addition.java
@@ -0,0 +1,7 @@
+public class Addition {
+    public static void main(String[] args) {
+        System.out.println("Hello Mr fadel!");
+        System.out.println("Hello are you feeling good ? \n \tThank");
+
+    }
+}
---
+++ src/ValueUser.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ValueUser {
+    public static void main( String[] arg) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("inserer une valeur");
+
+        int a=scanner.nextInt();
+
+        System.out.println("votre entre est  :" +  a);
+
+
+    }
+}
