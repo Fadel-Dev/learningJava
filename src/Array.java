8d08e87673ee1b4b1f9345324f7bde665e267ece
Fadel-Dev <diopfadel06@gmail.com>
2023-03-07 00:48:25 +0000

    creation array

---
+++ src/Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] student= {"anta", "cheikh", "fadel", "juliette", "modou"};
+
+        int i;
+        for (i=0; i<=student.length; i++)
+        {
+            System.out.println(student[i]);
+        }
+       
+
+    }
+}
